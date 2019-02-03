f47116e89680eee25f4a2c34f032b9630d4854ea
Guillermo Barila <gui365@yahoo.com.ar>
2019-02-03 17:51:30 -0500

    Fix article controller

--- controller/nytController.js
+++ controller/nytController.js
@@ -18,17 +18,8 @@ module.exports = {
     cleanParams(params);
     console.log(params);
 
-    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=53HNvZYAxoJUyhRF9aFUcOKhGtnqIWvX", { params })
+    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=[ADD-KEY-HERE]", { params })
       .then(data => (res.json(data.data.response.docs)))
-      // .then(data => console.log(data))
       .catch(err => console.log(err));
-    
-    // axios.get("https://rest.bandsintown.com/artists/beyonce?app_id=trilogy")
-    //   .then(data => console.log(data.data.name))
-    //   .catch(err => console.log(err));
-
-    // fetch("https://api.nytimes.com/svc/search/v2/articlesearch.json?api_key=b9f91d369ff59547cd47b931d8cbc56b:0:74623931")
-    // .then(res => console.log(res))
-    // .then(json => console.log(json))
   }
 };
