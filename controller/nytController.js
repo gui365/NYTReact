b042396fe7d166d48207193fffe181a990c41c7b
Guillermo Barila <gui365@yahoo.com.ar>
2019-02-04 21:24:43 -0500

    Revert update of packages

--- controller/nytController.js
+++ controller/nytController.js
@@ -1,5 +1,4 @@
 const axios = require("axios");
-const fetch = require("node-fetch");
 
 module.exports = {
   callNYT: function(req, res) {
@@ -18,7 +17,7 @@ module.exports = {
     cleanParams(params);
     console.log(params);
 
-    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=[ADD-API-KEY-HERE]", { params })
+    axios.get("https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=53HNvZYAxoJUyhRF9aFUcOKhGtnqIWvX", { params })
       .then(data => (res.json(data.data.response.docs)))
       .catch(err => console.log(err));
   }
